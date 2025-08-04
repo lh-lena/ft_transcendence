a7527081ff0a92dad54ae700069a6970dedf933f
Mo <mb_code@proton.me>
2025-08-04 11:57:24 +0200

    started adding tournament basics

---
+++ backend/src/modules/tournament/tournament.controller.ts
@@ -0,0 +1,50 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import Database from 'better-sqlite3';
+
+import type {
+  } from '../../schemas/tournament';
+
+import * as tournamentService from './tournament.service';
+
+export const tournamentController = { 
+
+  //controller to create an tournament
+  async create(
+    input: tournamentCreateInput,
+  ) : Promise< tournamentResponseType > {
+  	const ret = await tournamentService.create( input );
+    return ret ;
+  },
+  
+  //update tournament
+  async update(
+    id: tournamentIdInput,
+    input: tournamentUpdateInput,
+  ) : Promise< tournamentResponseType > {
+  	const ret = await tournamentService.update( id, input );
+    return ret;
+  },
+
+  //controller for tournament get All or by Id
+  async getAllorFiltered(
+    query: tournamentQueryInput,
+  ) : Promise< tournamentResponseArrayType > {
+    const ret =  await tournamentService.getQuery( query );
+    return ret;
+  },
+  
+  async getById(
+    id: tournamentIdInput,
+  ) : Promise< tournamentResponseType | null > {
+  	const ret = await tournamentService.getById( id );
+    return ret;
+  },
+  
+  //delete tournament
+ async remove(
+    id: tournamentIdInput,
+  ) : Promise< { message: string } > {
+  	const ret = await tournamentService.remove( id );
+    return ret;
+  },
+}
