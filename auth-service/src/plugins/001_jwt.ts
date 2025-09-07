f96fcdd86acf1136889013b72684f55e4b1d5354
Mo <mb_code@proton.me>
2025-09-07 19:39:43 +0200

    added jwt as plugins. TODO like last commit

--- auth-service/src/plugins/001_jwt.ts
+++ auth-service/src/plugins/001_jwt.ts
@@ -1,22 +1,48 @@
 import fp from 'fastify-plugin';
 import fastifyJwt from '@fastify/jwt';
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
 
 export default fp(async (fastify: FastifyInstance) => {
+  const accessSecret = process.env.ACCESS_TOKE_SECRET;
+  const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+
+  if (!accessSecret || !refreshSecret) {
+    throw new Error('JWT secrets are not defined in environment variables');
+  }
 
   fastify.register(async (instance) => {
     instance.register(fastifyJwt, {
-      secret: process.env.ACCESS_TOKEN_SECRET!,
+      secret: accessSecret,
       sign: { expiresIn: '15m' },
     });
 
-    instance.decorate("authenticate", async (request: any, reply: any) => {
-      try {
-        await request.jwtVerify();
-      } catch (err) {
+    instance.decorate(
+      'authenticateAccess',
+      async (request: FastifyRequest, reply: FastifyReply) => {
+        try {
+          await request.jwtVerify();
+        } catch (err) {
+          reply.code(401).send({ message: 'Unauthorized' });
+        }
+      },
+    );
 
-        fastify.register(fastifyJwt, {
-          secret: process.env.REFRESH_TOKEN_SECRET!,
-          sign: { expiresIn: '7d' },
-        }, { prefix: 'refresh' });
+    instance.decorate('generateAccessToken', (payload: object) => {
+      return instance.jwt.sign(payload);
+    });
+  });
+
+  fastify.register(
+    async (refresh) => {
+      refresh.register(fastifyJwt, {
+        secret: refreshSecret,
+        sign: { expiresIn: '7d' },
       });
+
+      refresh.decorate('generateRefreshToken', (payload: object) => {
+        return refresh.jwt.sign(payload);
+      });
+    },
+    { prefix: '/refresh' },
+  );
+});
