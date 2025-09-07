3643ec182a11491caeec879239fd1c384367110d
Olena <ohladkov@student.42berlin.de>
2025-09-07 12:23:09 +0200

    fix/realtime/api: update notify schema

--- realtime/src/api/routes/000_notification.routes.ts
+++ realtime/src/api/routes/000_notification.routes.ts
@@ -4,31 +4,27 @@ import type { RespondService } from '../../websocket/types/ws.types.js';
 import {
   NotificationRequestSchema,
   NotificationResponseSchema,
-  NotificationParamsSchema,
 } from '../../schemas/notification.schema.js';
-import type { NotificationParams, NotificationRequest } from '../../schemas/notification.schema.js';
+import type { NotificationRequest } from '../../schemas/notification.schema.js';
 import { processErrorLog } from '../../utils/error.handler.js';
 
 export const setupNotificationRoutes = (server: FastifyInstance): void => {
   const auth = server.auth as AuthService;
   const respond = server.respond as RespondService;
 
-  server.post('/notifications/:id', {
+  server.post('/notify', {
     preHandler: (request, reply, done) => auth.verifyServerOrigin(request, reply, done),
     schema: {
-      params: NotificationParamsSchema,
       body: NotificationRequestSchema,
       response: {
         200: NotificationResponseSchema,
+        500: NotificationResponseSchema,
       },
     },
     handler: (request: FastifyRequest, reply: FastifyReply) => {
       try {
-        const { id: userId } = request.params as NotificationParams;
-        const data = request.body as NotificationRequest;
-
-        const res = respond.notification(userId, data.tt, data.message);
-
+        const { reciver, payload, event } = request.body as NotificationRequest;
+        const res = respond.notification(reciver, event, payload.message);
         return reply.code(200).send({ success: res });
       } catch (error: unknown) {
         processErrorLog(
