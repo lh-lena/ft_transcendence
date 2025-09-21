97a1076a96469d0510caf080884df9868b0d64c4
Olena <ohladkov@student.42berlin.de>
2025-09-21 19:19:15 +0200

    feat/realtime: add game create route

--- realtime/src/plugins/api.plugin.ts
+++ realtime/src/plugins/api.plugin.ts
@@ -1,9 +1,11 @@
 import fp from 'fastify-plugin';
 import type { FastifyInstance, FastifyPluginCallback } from 'fastify';
 import { setupNotificationRoutes } from '../api/routes/000_notification.routes.js';
+import { setupGameRoutes } from '../api/routes/001_game.routes.js';
 
 const plugin: FastifyPluginCallback = (app: FastifyInstance) => {
   app.register(setupNotificationRoutes, { prefix: '/api' });
+  app.register(setupGameRoutes, { prefix: '/api' });
 };
 
 export const apiPlugin = fp(plugin, {
