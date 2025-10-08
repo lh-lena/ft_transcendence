4793147e04daf49b0c2ed1a2abf0ffa6dcafe625
Mo <mb_code@proton.me>
2025-10-08 14:36:01 +0200

    merged everything starts

--- auth-service/run_auth.sh
+++ auth-service/run_auth.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Hello MSC Eastman"
-
 source .env
-
-export ACCESS_TOKEN_SECRET=${JWT_SECRET:-$(openssl rand -base64 64)}
-export REFRESH_TOKEN_SECRET=${JWT_SECRET:-$(openssl rand -base64 64)}
 
 npm run dev
--- auth-service/src/routes/metric.route.ts
+++ auth-service/src/routes/metric.route.ts
@@ -1,34 +1,34 @@
 import { FastifyInstance, FastifyReply } from 'fastify';
 
 export default async function metricsRoutes(fastify: FastifyInstance) {
-  fastify.get(
-    '/metrics',
-    {
-      schema: {
-        response: {
-          200: {
-            type: 'string',
-            description: 'Prometheus metrics in text format',
-          },
-        },
-      },
-    },
-    async (_, reply: FastifyReply) => {
-      try {
-        const metrics = await fastify.metrics.register.metrics();
-        return reply
-          .header('Content-Type', fastify.metrics.register.contentType)
-          .code(200)
-          .send(metrics);
-      } catch {
-        fastify.log.error('Error generating metrics:');
-        return reply.code(500).send({
-          error: 'Failed to generate metrics',
-          timestamp: new Date().toISOString(),
-        });
-      }
-    },
-  );
+  //  fastify.get(
+  //    '/metrics',
+  //    {
+  //      schema: {
+  //        response: {
+  //          200: {
+  //            type: 'string',
+  //            description: 'Prometheus metrics in text format',
+  //          },
+  //        },
+  //      },
+  //    },
+  //    async (_, reply: FastifyReply) => {
+  //      try {
+  //        const metrics = await fastify.metrics.register.metrics();
+  //        return reply
+  //          .header('Content-Type', fastify.metrics.register.contentType)
+  //          .code(200)
+  //          .send(metrics);
+  //      } catch {
+  //        fastify.log.error('Error generating metrics:');
+  //        return reply.code(500).send({
+  //          error: 'Failed to generate metrics',
+  //          timestamp: new Date().toISOString(),
+  //        });
+  //      }
+  //    },
+  //  );
 
   // Optional: Add a ready endpoint for Kubernetes
   fastify.get('/ready', async (_, reply: FastifyReply) => {
