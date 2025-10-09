3fbf468049d21ce0a7205a5773b5e68add1b2b3b
Mo <mb_code@proton.me>
2025-10-09 14:32:46 +0200

    changed to realtime host -> do it via conf

--- backend/src/utils/realtime.ts
+++ backend/src/utils/realtime.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { gameType } from '../schemas/game';
 
 const wsApiClient = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'http://realtime:8081/api',
   timeout: 5000,
 });
 
