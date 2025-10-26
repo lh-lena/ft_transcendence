f915e0d4d232a3d228375164fc45937cd1de73ac
Lena <hladkovskaa@gmail.com>
2025-10-26 13:08:43 +0100

    fix/realtime: game_ready is sent propperly

--- realtime/src/websocket/services/respond.service.ts
+++ realtime/src/websocket/services/respond.service.ts
@@ -61,7 +61,6 @@ export default function createRespondService(app: FastifyInstance): RespondServi
     players.forEach((p) => {
       if (
         p.isAI !== true &&
-        game.playersReady.includes(p.userId) &&
         !excludeUsers.includes(p.userId)
       ) {
         userIds.push(p.userId);
